8d07fd96ff9655614cdae0b6049a3fbeebc7d56e
ivyycore <mariasequeira.dev@gmail.com>
2025-02-21 14:55:32 +0000

    Did the Percentagem exercise.

---
+++ Percentagem/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace Percentagem
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+        CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+        Console.WriteLine("Insere 4 números reais entre 0 e 1:");
+        string input1 = Console.ReadLine();
+        string input2 = Console.ReadLine();
+        string input3 = Console.ReadLine();
+        string input4 = Console.ReadLine();
+        
+        float input11 = float.Parse(input1);
+        float input22 = float.Parse(input2);
+        float input33 = float.Parse(input3);
+        float input44 = float.Parse(input4);
+
+        Console.WriteLine(input11);
+        Console.WriteLine(input22);
+        Console.WriteLine(input33);
+        Console.WriteLine(input44);
+
+        double input111 = input11;
+        double input222 = input22;   
+        double input333 = input33;   
+        double input444 = input44;
+        Console.WriteLine(input111.ToString("P", CultureInfo.InvariantCulture));
+        Console.WriteLine(input222.ToString("P", CultureInfo.InvariantCulture));
+        Console.WriteLine(input333.ToString("P", CultureInfo.InvariantCulture));
+        Console.WriteLine(input444.ToString("P", CultureInfo.InvariantCulture));
+
+
+
+
+        
+
+        
+
+        }
+    }
+}
